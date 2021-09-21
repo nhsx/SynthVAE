1ca9d03fd9bfd757a139d25d4ac6b17928b90ebe
Dan Schofield <daniel.schofield@nhsx.nhs.uk>
2021-09-21 14:34:00 +0100

    black and flake changes

--- scratch_vae_expts.py
+++ scratch_vae_expts.py
@@ -1,5 +1,4 @@
 # Standard imports
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -18,7 +17,7 @@ from opacus.utils.uniform_sampler import UniformWithReplacementSampler
 from pycox.datasets import support
 
 # For VAE dataset formatting
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import TensorDataset
 
 # VAE functions
 from VAE import Decoder, Encoder, VAE
@@ -41,25 +40,31 @@ my_seeds = np.random.randint(1e6, size=n_seeds)
 data_supp = support.read_df()
 
 ###############################################################################
-### DATA PREPROCESSING ###
+# DATA PREPROCESSING #
 # We one-hot the categorical cols and standardise the continuous cols
-data_supp['x14'] = data_supp['x0']
+data_supp["x14"] = data_supp["x0"]
 # data_supp = data_supp.astype('float32')
-data_supp = data_supp[['duration'] + [f'x{i}' for i in range(1,15)] + ['event']]
-data_supp[['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'event']] = data_supp[['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'event']].astype(int)
+data_supp = data_supp[
+    ["duration"] + [f"x{i}" for i in range(1, 15)] + ["event"]
+]
+data_supp[["x1", "x2", "x3", "x4", "x5", "x6", "event"]] = data_supp[
+    ["x1", "x2", "x3", "x4", "x5", "x6", "event"]
+].astype(int)
 transformer_dtypes = {
-        'i': 'one_hot_encoding',
-        'f': 'numerical',
-        'O': 'one_hot_encoding',
-        'b': 'one_hot_encoding',
-        'M': 'datetime',
-    }
+    "i": "one_hot_encoding",
+    "f": "numerical",
+    "O": "one_hot_encoding",
+    "b": "one_hot_encoding",
+    "M": "datetime",
+}
 ht = HyperTransformer(dtype_transformers=transformer_dtypes)
 ht.fit(data_supp)
 transformed = ht.transform(data_supp)
-cat_cols = [f'x{i}' for i in range(1,7)] + ['event']
-cont_cols = [f'x{i}' for i in range(7,15)] + ['duration']
-num_categories = (np.array([np.amax(data_supp[col]) for col in cat_cols]) + 1).astype(int)
+cat_cols = [f"x{i}" for i in range(1, 7)] + ["event"]
+cont_cols = [f"x{i}" for i in range(7, 15)] + ["duration"]
+num_categories = (
+    np.array([np.amax(data_supp[col]) for col in cat_cols]) + 1
+).astype(int)
 num_continuous = len(cont_cols)
 cols_standardize = transformed.columns[:num_continuous]
 cols_leave = transformed.columns[num_continuous:]
@@ -68,28 +73,31 @@ leave = [([col], None) for col in cols_leave]
 x_mapper = DataFrameMapper(leave + standardize)
 x_train_df = x_mapper.fit_transform(transformed)
 x_train_df = x_mapper.transform(transformed)
-x_train = x_train_df.astype('float32')
+x_train = x_train_df.astype("float32")
 ###############################################################################
 # Prepare data for interaction with torch VAE
 Y = torch.Tensor(x_train)
 dataset = TensorDataset(Y)
 batch_size = 32
 
-generator=None
-sample_rate = batch_size/len(dataset)
+generator = None
+sample_rate = batch_size / len(dataset)
 data_loader = torch.utils.data.DataLoader(
-      dataset,
-      batch_sampler=UniformWithReplacementSampler(
-          num_samples=len(dataset),
-          sample_rate=sample_rate,
-          generator=generator
-      ),
-      pin_memory=True,
-      generator=generator
-  )
+    dataset,
+    batch_sampler=UniformWithReplacementSampler(
+        num_samples=len(dataset), sample_rate=sample_rate, generator=generator
+    ),
+    pin_memory=True,
+    generator=generator,
+)
 
 # shuffle = True
-# data_loader = DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=shuffle)
+# data_loader = DataLoader(
+#   dataset,
+#   batch_size=batch_size,
+#   pin_memory=True,
+#   shuffle=shuffle
+# )
 
 # Define lists to contain the metrics achieved on the
 # train/generate/evaluate runs
@@ -108,81 +116,101 @@ svr_privs = []
 gowers = []
 
 target_delta = 1e-3
-target_eps = 10.
+target_eps = 10.0
 
 for i in range(1):
-  print(f"Train+Generate+Evaluate Run {i+1}/{n_seeds}")
-  set_seed(my_seeds[i])
+    print(f"Train+Generate+Evaluate Run {i+1}/{n_seeds}")
+    set_seed(my_seeds[i])
 
-  # Create VAE
-  latent_dim = 2
-  encoder = Encoder(x_train.shape[1], latent_dim)
-  decoder = Decoder(latent_dim, num_continuous, num_categories=num_categories)
-  vae = VAE(encoder, decoder)
+    # Create VAE
+    latent_dim = 2
+    encoder = Encoder(x_train.shape[1], latent_dim)
+    decoder = Decoder(latent_dim, num_continuous, num_categories=num_categories)
+    vae = VAE(encoder, decoder)
 
-  vae.train(data_loader, n_epochs=60)
+    vae.train(data_loader, n_epochs=60)
 
-  # vae.diff_priv_train(data_loader, n_epochs=60, C=10, target_eps=target_eps, target_delta=target_delta, sample_rate=sample_rate)
-  # print(vae.get_privacy_spent(target_delta))
+    # vae.diff_priv_train(
+    #   data_loader,
+    #   n_epochs=60,
+    #   C=10,
+    #   target_eps=target_eps,
+    #   target_delta=target_delta,
+    #   sample_rate=sample_rate
+    # )
+    # print(vae.get_privacy_spent(target_delta))
 
-  #  Collect samples and transform them out of one-hot, standardised form
-  samples_ = vae.generate(data_supp.shape[0]).detach().numpy()
-  samples = np.ones_like(samples_)
-  samples[:,:num_continuous] = samples_[:,-num_continuous:]
-  samples[:,num_continuous:] = samples_[:,:-num_continuous]
-  samples = pd.DataFrame(samples)
-  samples.columns = transformed.columns
-  samples = ht.reverse_transform(samples)
-  t = x_mapper.transform(transformed)
-  t_ = np.ones_like(t)
-  t_[:,:num_continuous] = t[:,-num_continuous:]
-  t_[:,num_continuous:] = t[:,:-num_continuous]
-  t_ = pd.DataFrame(t_)
-  t_.columns = transformed.columns
-  t_ = ht.reverse_transform(t_)
-  samples[cat_cols] = samples[cat_cols].astype(object)
-  t_[cat_cols] = t_[cat_cols].astype(object)
-  for l in x_mapper.features:
-    if l[0][0] in cont_cols:
-      f = l[0][0]
-      samples[f] = l[1].inverse_transform(samples[f])
-      t_[f] = l[1].inverse_transform(t_[f])
+    #  Collect samples and transform them out of one-hot, standardised form
+    samples_ = vae.generate(data_supp.shape[0]).detach().numpy()
+    samples = np.ones_like(samples_)
+    samples[:, :num_continuous] = samples_[:, -num_continuous:]
+    samples[:, num_continuous:] = samples_[:, :-num_continuous]
+    samples = pd.DataFrame(samples)
+    samples.columns = transformed.columns
+    samples = ht.reverse_transform(samples)
+    t = x_mapper.transform(transformed)
+    t_ = np.ones_like(t)
+    t_[:, :num_continuous] = t[:, -num_continuous:]
+    t_[:, num_continuous:] = t[:, :-num_continuous]
+    t_ = pd.DataFrame(t_)
+    t_.columns = transformed.columns
+    t_ = ht.reverse_transform(t_)
+    samples[cat_cols] = samples[cat_cols].astype(object)
+    t_[cat_cols] = t_[cat_cols].astype(object)
+    for feature in x_mapper.features:
+        if feature[0][0] in cont_cols:
+            f = feature[0][0]
+            samples[f] = feature[1].inverse_transform(samples[f])
+            t_[f] = feature[1].inverse_transform(t_[f])
 
-  evals = evaluate(samples, t_, aggregate=False)
-  bns.append(np.array(evals['raw_score'])[0])
-  lrs.append(np.array(evals['raw_score'])[1])
-  svcs.append(np.array(evals['raw_score'])[2])
-  gmlls.append(np.array(evals['raw_score'])[3])
-  cs.append(np.array(evals['raw_score'])[4])
-  ks.append(np.array(evals['raw_score'])[5])
-  kses.append(np.array(evals['raw_score'])[6])
-  contkls.append(np.array(evals['raw_score'])[7])
-  disckls.append(np.array(evals['raw_score'])[8])
-  gowers.append(np.mean(gower.gower_matrix(t_, samples)))
+    evals = evaluate(samples, t_, aggregate=False)
 
-  lr_priv = NumericalLR.compute(
-      t_.fillna(0),
-      samples.fillna(0),
-      key_fields=([f'x{i}' for i in range(1,data_supp.shape[1]-2)] + ['event'] + ['duration']),
-      sensitive_fields=['x14']
+    bns.append(np.array(evals["raw_score"])[0])
+    lrs.append(np.array(evals["raw_score"])[1])
+    svcs.append(np.array(evals["raw_score"])[2])
+    gmlls.append(np.array(evals["raw_score"])[3])
+    cs.append(np.array(evals["raw_score"])[4])
+    ks.append(np.array(evals["raw_score"])[5])
+    kses.append(np.array(evals["raw_score"])[6])
+    contkls.append(np.array(evals["raw_score"])[7])
+    disckls.append(np.array(evals["raw_score"])[8])
+    gowers.append(np.mean(gower.gower_matrix(t_, samples)))
+
+    lr_priv = NumericalLR.compute(
+        t_.fillna(0),
+        samples.fillna(0),
+        key_fields=(
+            [f"x{i}" for i in range(1, data_supp.shape[1] - 2)]
+            + ["event"]
+            + ["duration"]
+        ),
+        sensitive_fields=["x14"],
     )
-  lr_privs.append(lr_priv)
+    lr_privs.append(lr_priv)
 
-  mlp_priv = NumericalMLP.compute(
-    t_.fillna(0),
-    samples.fillna(0),
-    key_fields=([f'x{i}' for i in range(1,data_supp.shape[1]-2)] + ['event'] + ['duration']),
-    sensitive_fields=['x14']
-  )
-  mlp_privs.append(mlp_priv)
+    mlp_priv = NumericalMLP.compute(
+        t_.fillna(0),
+        samples.fillna(0),
+        key_fields=(
+            [f"x{i}" for i in range(1, data_supp.shape[1] - 2)]
+            + ["event"]
+            + ["duration"]
+        ),
+        sensitive_fields=["x14"],
+    )
+    mlp_privs.append(mlp_priv)
 
-  svr_priv = NumericalSVR.compute(
-    t_.fillna(0),
-    samples.fillna(0),
-    key_fields=([f'x{i}' for i in range(1,data_supp.shape[1]-2)] + ['event'] + ['duration']),
-    sensitive_fields=['x14']
-  )
-  svr_privs.append(svr_priv)
+    svr_priv = NumericalSVR.compute(
+        t_.fillna(0),
+        samples.fillna(0),
+        key_fields=(
+            [f"x{i}" for i in range(1, data_supp.shape[1] - 2)]
+            + ["event"]
+            + ["duration"]
+        ),
+        sensitive_fields=["x14"],
+    )
+    svr_privs.append(svr_priv)
 
 bns = np.array(bns)
 lrs = np.array(lrs)
@@ -194,6 +222,7 @@ kses = np.array(kses)
 contkls = np.array(contkls)
 disckls = np.array(disckls)
 gowers = np.array(gowers)
+
 print(f"BN: {np.mean(bns)} +/- {np.std(bns)}")
 print(f"LR: {np.mean(lrs)} +/- {np.std(lrs)}")
 print(f"SVC: {np.mean(svcs)} +/- {np.std(svcs)}")
