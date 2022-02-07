aeabe7abcafb465e1d9b67f075f66a0d277c5d42
David Brind <33844002+DocSchrodinger@users.noreply.github.com>
2022-02-07 17:37:43 +0000

    Transformer dictionary substitution for hypertransformer
    Added in loops for a dictionary of continuous and categorical transformers - This allows to get round the broken hypertransformers for now

--- Test_Runs_SynthVAE2_ENV.py
+++ Test_Runs_SynthVAE2_ENV.py
@@ -55,6 +55,47 @@ data_supp[["x1", "x2", "x3", "x4", "x5", "x6", "event"]] = data_supp[
 # transformers take a 'columns' argument that can only allow for fitting of one column - so you need to loop over and create one for each column
 # in order to fit the dataset - https://github.com/sdv-dev/RDT/issues/376
 
+from rdt.transformers import categorical, numerical, boolean, datetime
+
+continuous_transformers = {}
+categorical_transformers = {}
+boolean_transformers = {}
+datetime_transformers = {}
+
+continuous_columns = ['duration'] + [f"x{i}" for i in range(7,15)]
+categorical_columns = ['event'] + [f"x{i}" for i in range(1,7)] 
+num_categories = (
+    np.array([np.amax(data_supp[col]) for col in categorical_columns]) + 1
+).astype(int)
+
+transformed_dataset = data_supp
+
+# Define columns based on datatype and then loop over creating and fitting transformers
+
+# Do continuous first via GMM as it gives a mixture column that then needs to be encoded OHE
+for index, column in enumerate(continuous_columns):
+
+    temp_continuous = numerical.BayesGMMTransformer()
+    temp_continuous.fit(transformed_dataset, columns = column)
+    continuous_transformers['continuous_{}'.format(index)] = temp_continuous
+
+    transformed_dataset = temp_continuous.transform(transformed_dataset)
+
+    # Each numerical one gets a .normalized column + a .component column giving the mixture info
+    # This too needs to be one hot encoded
+
+    categorical_columns += [str(column) + '.component']
+
+for index, column in enumerate(categorical_columns):
+
+    temp_categorical = categorical.OneHotEncodingTransformer()
+    temp_categorical.fit(transformed_dataset, columns = column)
+    categorical_transformers['categorical_{}'.format(index)] = temp_categorical
+
+    transformed_dataset = temp_categorical.transform(transformed_dataset)
+
+# Now we need to remove any duplicate columns within the dataset post-processing
+
 transformed = ht.transform(data_supp)
 cat_cols = [f"x{i}" for i in range(1, 7)] + ["event"]
 cont_cols = [f"x{i}" for i in range(7, 15)] + ["duration"]
