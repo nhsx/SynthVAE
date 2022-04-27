e05747849321a7cc31e9d459e0984a96872a7acd
David Brind <david.brind@hdruk.ac.uk>
2022-04-27 14:52:05 +0100

    bringing in current heart work
    - adapted utils to handle the heart dataset (and in future any dataset)
    - Brought in kaggle data as well as my synthetic runs using Heart_Pre_Processing.ipynb
    - Brought in pre-processing notebook to train and create synthetic sets
    - Brought in heart_analysis notebook to perform classification using decision tree model

--- utils.py
+++ utils.py
@@ -16,10 +16,168 @@ def set_seed(seed):
     torch.manual_seed(seed)
 
 
-# -------- Pre-Processing for SUPPORT -------- #
-
 gmm_seed = 0
 
+def general_pre_proc(data_supp, user_categorical_columns, user_continuous_columns, user_datetime_columns, pre_proc_method):
+
+    # Specify column configurations
+
+    original_categorical_columns = user_categorical_columns
+    original_continuous_columns = user_continuous_columns
+    original_datetime_columns = user_datetime_columns
+
+    categorical_columns = original_categorical_columns.copy()
+    continuous_columns = original_continuous_columns.copy()
+    datetime_columns = original_datetime_columns.copy()
+
+    # As of coding this, new version of RDT adds in GMM transformer which is what we require, however hyper transformers do not work as individual
+    # transformers take a 'columns' argument that can only allow for fitting of one column - so you need to loop over and create one for each column
+    # in order to fit the dataset - https://github.com/sdv-dev/RDT/issues/376
+
+    continuous_transformers = {}
+    categorical_transformers = {}
+    datetime_transformers = {}
+
+    # num_categories is either the maximum number within the categorical
+    # column, or the number of unique string terms
+
+    num_categories = []
+
+    for col in original_categorical_columns:
+
+        if data_supp[col].dtype == float:
+
+            # Convert to int
+            data_supp[col] = data_supp[col].astype(int)
+
+        if data_supp[col].dtype == int:
+
+            num_categories.append(np.amax(data_supp[col]) + 1)
+
+        # Categories are strings
+        else:
+
+            # Convert column into one type
+            values = np.unique(data_supp[col].astype(str), return_counts=False)
+            num_categories.append(values.shape[0])
+
+    num_continuous = len(original_continuous_columns)
+
+    transformed_dataset = data_supp
+
+    # Define columns based on datatype and then loop over creating and fitting
+    # transformers
+
+    # Do datetime columns first to convert to seconds
+
+    for index, column in enumerate(original_datetime_columns):
+
+        # Fit datetime transformer - converts to seconds
+        temp_datetime = DatetimeTransformer()
+        temp_datetime.fit(transformed_dataset, columns=column)
+        datetime_transformers["datetime_{}".format(column)] = temp_datetime
+
+        transformed_dataset = temp_datetime.transform(transformed_dataset)
+        # These newly fitted datetime columns now need to be scaled
+        # And treated as a continuous variable
+        continuous_columns += [str(column) + ".value"]
+
+    # WE NEED TO RETAIN THIS SET AS METRICS DO NOT EVALUATE WITH DATETIMES BUT THEY WILL EVALUATE
+    # IF DATETIMES ARE IN A SECONDS FORMAT
+
+    original_metric_set = transformed_dataset.copy()
+
+    if pre_proc_method == "GMM":
+
+        for index, column in enumerate(continuous_columns):
+
+            # Fit GMM
+            temp_continuous = numerical.BayesGMMTransformer(random_state=gmm_seed)
+            temp_continuous.fit(transformed_dataset, columns=column)
+            continuous_transformers["continuous_{}".format(column)] = temp_continuous
+
+            categorical_columns += [str(column) + ".component"]
+
+            transformed_dataset = temp_continuous.transform(transformed_dataset)
+
+        # Each numerical one gets a .normalized column + a .component column giving the mixture info
+        # This too needs to be one hot encoded
+
+        continuous_columns = [str(col) + ".normalized" for col in continuous_columns]
+
+    elif pre_proc_method == "standard":
+
+        for index, column in enumerate(continuous_columns):
+
+            # Fit sklearn standard scaler to each column
+            temp_continuous = StandardScaler()
+            temp_column = transformed_dataset[column].values.reshape(-1, 1)
+            temp_continuous.fit(temp_column)
+            continuous_transformers["continuous_{}".format(column)] = temp_continuous
+
+            transformed_dataset[column] = (
+                temp_continuous.transform(temp_column)
+            ).flatten()
+
+    num_categories = []
+
+    for col in categorical_columns:
+
+        if transformed_dataset[col].dtype == float:
+
+            # Convert to int
+            transformed_dataset[col] = transformed_dataset[col].astype(int)
+
+        if transformed_dataset[col].dtype == int:
+
+            num_categories.append(np.amax(transformed_dataset[col]) + 1)
+
+        # Categories are strings/objects
+        else:
+
+            # Convert column into one type
+            values = np.unique(
+                transformed_dataset[col].astype(str), return_counts=False
+            )
+
+            num_categories.append(values.shape[0])
+
+    num_continuous = len(continuous_columns)
+
+    for index, column in enumerate(categorical_columns):
+
+        temp_categorical = categorical.OneHotEncodingTransformer()
+        temp_categorical.fit(transformed_dataset, columns=column)
+        categorical_transformers["categorical_{}".format(index)] = temp_categorical
+
+        transformed_dataset = temp_categorical.transform(transformed_dataset)
+
+    # We need the dataframe in the correct format i.e. categorical variables first and in the order of
+    # num_categories with continuous variables placed after
+
+    reordered_dataframe = pd.DataFrame()
+
+    reordered_dataframe = transformed_dataset.iloc[:, num_continuous:]
+
+    reordered_dataframe = pd.concat(
+        [reordered_dataframe, transformed_dataset.iloc[:, :num_continuous]], axis=1
+    )
+
+    x_train_df = reordered_dataframe.to_numpy()
+    x_train = x_train_df.astype("float32")
+
+    return (
+        x_train,
+        original_metric_set,
+        reordered_dataframe.columns,
+        continuous_transformers,
+        categorical_transformers,
+        datetime_transformers,
+        num_categories,
+        num_continuous,
+    )
+
+# -------- Pre-Processing for SUPPORT -------- #
 
 def support_pre_proc(data_supp, pre_proc_method="GMM"):
 
@@ -144,7 +302,7 @@ def mimic_pre_proc(data_supp, pre_proc_method="GMM"):
         "VALUEUOM",
         "LABEL",
     ]
-    original_continuous_columns = ["SUBJECT_ID", "VALUE", "age"]
+    original_continuous_columns = ["Unnamed: 0", "ROW_ID", "SUBJECT_ID", "VALUE", "age"]
     original_datetime_columns = ["ADMITTIME", "DISCHTIME", "DOB", "CHARTTIME"]
 
     categorical_columns = original_categorical_columns.copy()
